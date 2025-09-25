c195b6ccfd68fe798615944906930233a8078661
Uriel Correia <uriel2405correia@gmail.com>
2025-09-25 21:16:55 +0000

    Corrigir

---
+++ Javascript/Entrada de valores/entr_val.js
@@ -0,0 +1,14 @@
+function addedTask(){
+    // let message = "Task added successfully";
+
+    let input_task = document.getElementById("input_task");
+    let task = input_task.value;
+    //Para salvar o valor de um input é necessário fazer isso, se não vai receber a própria função input como valor
+
+    document.getElementById("message").textContent = task;
+}
+
+function input_task(){
+
+
+}
