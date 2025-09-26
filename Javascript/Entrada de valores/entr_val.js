85f2d072e623f9163b548a92c978812da784b7f7
Uriel Correia <uriel2405correia@gmail.com>
2025-09-26 18:56:37 +0000

    Corrected

--- Javascript/Entrada de valores/entr_val.js
+++ Javascript/Entrada de valores/entr_val.js
@@ -4,11 +4,15 @@ function addedTask(){
     let input_task = document.getElementById("input_task");
     let task = input_task.value;
     //Para salvar o valor de um input é necessário fazer isso, se não vai receber a própria função input como valor
-
     document.getElementById("message").textContent = task;
-}
 
-function input_task(){
+    //adicionando uma lista de atividades
+    let task_list = document.getElementById("task_list");
+    let new_task = document.createElement("li");
 
+    new_task.textContent = task;
 
+    task_list.appendChild(new_task);
+
+    input_task.value = "";
 }
